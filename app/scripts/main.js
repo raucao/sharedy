2e6f05594e00def88026f88b1a6ee16477d73340
Sebastian Kippe <sebastian@kip.pe>
2013-03-27 17:03:14 +0000

    Don't use global namespace, fix bug that prevented uploading after loading history and going back

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -42,6 +42,13 @@ app.on('show-history', function() {
   app.loadHistory();
 });
 
+app.on('show-share', function() {
+  app.imageFiles = [];
+  $("#upload button.cancel").click(app.helpers.cancelSharing);
+  $("#upload button.upload").click(app.helpers.uploadImages);
+  app.helpers.initializeDragndrop();
+});
+
 app.loadHistory = function () {
   remoteStorage.sharedy.getListing('images/').then(function(listing) {
     var items = listing.sort();
@@ -88,6 +95,151 @@ app.helpers.renderHistoryItem = function (list, item) {
   list.append(template);
 };
 
+app.helpers.initializeDragndrop = function () {
+  $('#dropzone').bind('dragenter', function() {
+    $("p.placeholder").hide();
+    $(this).addClass('active');
+    return false;
+  });
+
+  $('#dropzone').bind('dragover', function (e) {
+    e.preventDefault();
+    return false;
+  });
+
+  $('#dropzone').bind('dragleave', function() {
+    $("p.placeholder").show();
+    $(this).removeClass('active');
+    return false;
+  });
+
+  $('#dropzone').bind('drop', function(e) {
+    $(this).removeClass('active');
+    $("#dropzone").hide();
+    $("#upload").show();
+
+    e.preventDefault();
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    }
+
+    var files = e.dataTransfer.files;
+
+    loadFiles(files);
+  });
+
+  $('input#file-picker').bind('change', function(e) {
+    if (e.target.files.length == 0) {
+      return false
+    }
+
+    $("#dropzone").hide();
+    $("#upload").show();
+
+    files = e.target.files;
+
+    app.helpers.loadFiles(files);
+  });
+};
+
+app.helpers.cancelSharing = function () {
+  $('#upload').hide();
+  $('#dropzone').show();
+  $("p.placeholder").show();
+  $('#dropped-files > div.image').remove();
+  $("#upload button.upload").removeAttr('disabled').show();
+  $("#upload button.cancel").html('cancel');
+
+  app.imageFiles = [];
+
+  return false;
+}
+
+app.helpers.uploadImages = function () {
+  $.each(app.imageFiles, function(index, file){
+    var imgEl = $(".image[data-filename='"+window.btoa(file.name)+"']");
+    var timestamp = moment().format("YYMMDD-HHmmss-");
+    var filename  = timestamp + file.name;
+
+    remoteStorage.sharedy.storeImage(
+      file.type,
+      filename,
+      file.data,
+      function(){
+        // success
+        app.helpers.removeUploadIndicator(imgEl);
+        app.helpers.showImageUrl(imgEl, remoteStorage.sharedy.getImageUrl(filename));
+        $("#upload button.upload").hide();
+        // failure
+        // $("#upload button.upload").removeAttr('disabled');
+      }
+    );
+
+    app.helpers.showUploadIndicator(imgEl);
+    $("#upload button.upload").attr('disabled', 'disabled');
+    $("#upload button.cancel").html('Back');
+  });
+}
+
+app.helpers.showUploadIndicator = function (element) {
+  var html = '<div class="overlay loading"></div>';
+  $(element).append($(html));
+  $(element).find('.overlay.loading').spin('large', 'white');
+}
+
+app.helpers.removeUploadIndicator = function (element) {
+  $(element).find('.overlay.loading').remove();
+}
+
+app.helpers.showImageUrl = function (element, url) {
+  var html = '\
+    <div class="overlay result">\
+      <label for="direct-url">Image URL:</label>\
+      <input type="text" name="direct-url" value="'+url+'" onclick="this.select()">\
+    </div>';
+
+  $(element).append($(html));
+}
+
+app.helpers.validateFileType = function (fileType) {
+  if (!fileType.match('image.*')) {
+    alert('Hey! Images only');
+    $("p.placeholder").show();
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+app.helpers.loadFiles = function (files) {
+  $.each(files, function(index, file) {
+    if (!app.helpers.validateFileType(files[index].type)) {
+      return;
+    }
+
+    var fileReaderBase64 = new FileReader();
+    var fileReaderBinary = new FileReader();
+
+    fileReaderBase64.onload = (function(file) {
+      return function(e) {
+        var html = '<div class="image" data-filename='+window.btoa(file.name)+'>';
+            html += '<img src='+this.result+'></div>';
+        $('#dropped-files').append(html);
+      };
+    })(files[index]);
+
+    fileReaderBinary.onload = (function(file) {
+      return function(e) {
+        app.imageFiles.push({name: file.name, type: file.type, data: this.result});
+      };
+    })(files[index]);
+
+    fileReaderBase64.readAsDataURL(file);
+    fileReaderBinary.readAsArrayBuffer(file);
+  });
+}
+
 $(function() {
   app.views.show('share');
   $("#app-overlay").show();
@@ -111,158 +263,6 @@ $(function() {
     });
 
     jQuery.event.props.push('dataTransfer');
-
-    var imageFiles = [];
-
-    $('#dropzone').bind('dragenter', function() {
-      $("p.placeholder").hide();
-      $(this).addClass('active');
-      return false;
-    });
-
-    $('#dropzone').bind('dragover', function (e) {
-      e.preventDefault();
-      return false;
-    });
-
-    $('#dropzone').bind('dragleave', function() {
-      $("p.placeholder").show();
-      $(this).removeClass('active');
-      return false;
-    });
-
-    $('#dropzone').bind('drop', function(e) {
-      $(this).removeClass('active');
-      $("#dropzone").hide();
-      $("#upload").show();
-
-      e.preventDefault();
-      if (e.stopPropagation) {
-        e.stopPropagation();
-      }
-
-      var files = e.dataTransfer.files;
-
-      loadFiles(files);
-    });
-
-    $('input#file-picker').bind('change', function(e) {
-      if (e.target.files.length == 0) {
-        return false
-      }
-
-      $("#dropzone").hide();
-      $("#upload").show();
-
-      files = e.target.files;
-
-      loadFiles(files);
-    });
-
-    function loadFiles(files) {
-      $.each(files, function(index, file) {
-        if (!validateFileType(files[index].type)) {
-          return;
-        }
-
-        var fileReaderBase64 = new FileReader();
-        var fileReaderBinary = new FileReader();
-
-        fileReaderBase64.onload = (function(file) {
-          return function(e) {
-            var html = '<div class="image" data-filename='+window.btoa(file.name)+'>';
-                html += '<img src='+this.result+'></div>';
-            $('#dropped-files').append(html);
-          };
-        })(files[index]);
-
-        fileReaderBinary.onload = (function(file) {
-          return function(e) {
-            imageFiles.push({name: file.name, type: file.type, data: this.result});
-          };
-        })(files[index]);
-
-        fileReaderBase64.readAsDataURL(file);
-        fileReaderBinary.readAsArrayBuffer(file);
-      });
-    }
-
-    function validateFileType(fileType) {
-      if (!fileType.match('image.*')) {
-        alert('Hey! Images only');
-        $("p.placeholder").show();
-        return false;
-      }
-      else {
-        return true;
-      }
-    }
-
-    function cancelSharing() {
-      $('#upload').hide();
-      $('#dropzone').show();
-      $("p.placeholder").show();
-      $('#dropped-files > div.image').remove();
-      $("#upload button.upload").removeAttr('disabled').show();
-      $("#upload button.cancel").html('cancel');
-
-      imageFiles = [];
-
-      return false;
-    }
-
-    $("#upload button.cancel").on("click", cancelSharing);
-    $("#upload button.upload").on("click", uploadImages);
-
-    function uploadImages() {
-      $.each(imageFiles, function(index, file){
-        var imgEl = imageElement(file.name)
-        var timestamp = moment().format("YYMMDD-HHmmss-");
-        var filename  = timestamp + file.name;
-
-        remoteStorage.sharedy.storeImage(
-          file.type,
-          filename,
-          file.data,
-          function(){
-            // success
-            removeUploadIndicator(imgEl);
-            showImageUrl(imgEl, remoteStorage.sharedy.getImageUrl(filename));
-            $("#upload button.upload").hide();
-            // failure
-            // $("#upload button.upload").removeAttr('disabled');
-          }
-        );
-
-        showUploadIndicator(imgEl);
-        $("#upload button.upload").attr('disabled', 'disabled');
-        $("#upload button.cancel").html('Back');
-      });
-    }
-
-    function showUploadIndicator(element) {
-      var html = '<div class="overlay loading"></div>';
-      $(element).append($(html));
-      $(element).find('.overlay.loading').spin('large', 'white');
-    }
-
-    function removeUploadIndicator(element) {
-      $(element).find('.overlay.loading').remove();
-    }
-
-    function showImageUrl(element, url) {
-      var html = '\
-        <div class="overlay result">\
-          <label for="direct-url">Image URL:</label>\
-          <input type="text" name="direct-url" value="'+url+'" onclick="this.select()">\
-        </div>';
-
-      $(element).append($(html));
-    }
-
-    function imageElement(filename) {
-      return $(".image[data-filename='"+window.btoa(filename)+"']");
-    }
 
     $("[data-nav-item='history'] a").tooltip();
 
